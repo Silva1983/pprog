18f08034a6ef6214688654fcfa1a70aaa7e49cc1
António Silva <1171343@isep.ipp.pt>
2019-07-07 18:18:42 +0100

    Questão 3

--- Exames/exameEpNormal2019/src/main/java/com/mycompany/exameepnormal2019/ListaImoveis.java
+++ Exames/exameEpNormal2019/src/main/java/com/mycompany/exameepnormal2019/ListaImoveis.java
@@ -5,10 +5,63 @@
  */
 package com.mycompany.exameepnormal2019;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ssilv
  */
 public class ListaImoveis {
     
+    private List<Imovel>imoveis;
+    
+    public ListaImoveis(){
+        this.imoveis = new ArrayList<>();
+    }
+    
+    public ListaImoveis(List<Imovel>imoveis){
+        this.imoveis = new ArrayList<>(imoveis);
+    }
+    
+    public ArrayList<Imovel>getImoveis(){
+        return new ArrayList<>(imoveis);
+    }
+    
+    public void setImoveis(List<Imovel>novaListaImoveis){
+        imoveis = new ArrayList<>(novaListaImoveis);
+    }
+    
+    public void eliminarImovel(Imovel removeAtleta){
+        imoveis.remove(removeAtleta);
+    }
+   /* 
+    public List<Imovel>getOrdenacaoPorLocalidade(){
+        
+    }
+    */
+    
+    public void calcularImoveis(ArrayList<Imovel>imoveis){
+        for(Imovel i: imoveis){
+            System.out.println(i.getImoveis().toString() + i.valorImovel());
+        }
+    }
+    
+    public boolean exportarParaFicheiro(ArrayList<Imovel> obj, File ficheiro)throws IOException{
+        try{
+            ObjectOutputStream objImovel = new ObjectOutputStream(new FileOutputStream(ficheiro));
+            try{
+                objImovel.writeObject(obj);   
+            }finally{
+                objImovel.close();
+            }
+            return true;
+        }catch(IOException e){
+            return false;
+        }
+    }
 }
